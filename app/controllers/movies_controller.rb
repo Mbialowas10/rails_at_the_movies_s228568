3d8158c010a98514982a2b15ab053a13dd27f174
Michael Bialowas <mbialowas@rrc.ca>
2022-09-28 10:41:11 -0500

    Final Search Form

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,14 +2,17 @@ class MoviesController < ApplicationController
   def index
     #@movies = Movie.order("average_vote DESC")
     @movies = Movie.includes(:production_company).all.order("average_vote DESC")
+
+
   end
 
   def show
     @movie = Movie.find(params[:id])
   end
   def search
-    wildcard_search ="% #{params[:keywords]} %"
+    wildcard_search = "%#{params[:keywords]}%"
     #debugger.log(wildcard_search)
     @movies = Movie.where("title LIKE ?", wildcard_search)
   end
+
 end
